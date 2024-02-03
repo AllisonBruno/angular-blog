7be8046bfe497227ee3b3a4a64b6fc496a240177
Allison Bruno <allbrunofn@gmail.com>
2024-02-03 15:45:10 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "saiu novo filme ",
+    "description": "ola",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT8iw6-OvihXwXXT9l5K20mQQj4DU3bMFwfQ&usqp=CAU"
+  },
+  {
+    "id": "2",
+    "title": "saiu novo filme ",
+    "description": "ola",
+    "photoCover":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Marvel-tv-logo.svg/2560px-Marvel-tv-logo.svg.png"
+  },
+  {
+    "id": "3",
+    "title": "saiu novo filme ",
+    "description": "ola",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkHGkdaVAQn64ETv8tFV7_t4CQJ13vuNgjQQ&usqp=CAU"
+  },
+  {
+    "id": "4",
+    "title": "saiu novo filme ",
+    "description": "ola",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyKsUYrGk9jiP6oFsNja7sFINxZNNtdn5XuQ&usqp=CAU"
+  }
+]
